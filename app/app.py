bab5a16ba52b51d592da49d2f0958bde4cdde5d8
VijayV28 <vijayv2807@gmail.com>
2024-07-06 07:49:19 +0530

    Machine Learning component added

--- app/app.py
+++ app/app.py
@@ -12,6 +12,15 @@ import pandas as pd
 from langchain_openai import OpenAI, ChatOpenAI
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 
+from langchain.prompts import PromptTemplate
+from langchain.chains.llm import LLMChain
+from langchain.chains.sequential import SequentialChain, SimpleSequentialChain
+from langchain_experimental.agents.agent_toolkits import create_python_agent
+from langchain_experimental.tools.python.tool import PythonREPLTool
+from langchain.agents.agent_types import AgentType
+from langchain.utilities import WikipediaAPIWrapper
+from langchain.utilities import GoogleSearchAPIWrapper
+
 # Main
 st.title("Data Lens: Your AI-powered data analysis assistant")
 st.write(
@@ -60,7 +69,7 @@ if st.session_state.clicked[1]:
             steps = llm(
                 "What are the steps of exploratory data analysis? Give a short and concise summary."
             )
-            return steps
+            return steps.content
 
         # Initializing the agent
         pandas_agent = create_pandas_dataframe_agent(
@@ -70,7 +79,7 @@ if st.session_state.clicked[1]:
         # Main Functions
         @st.cache_data
         def function_agent():
-            st.write("**Data Overview")
+            st.write("**Data Overview**")
 
             st.write("Sample from the data:")
             st.write(df.head())
@@ -81,12 +90,10 @@ if st.session_state.clicked[1]:
             )
             st.write(columns_meaning)
 
-            missing_values = pandas_agent.run(
-                "Are there any missing values in the dataset?"
-            )
+            missing_values = pandas_agent.run("Check for missing values in the dataset")
             st.write(missing_values)
 
-            duplicates = pandas_agent.run("Are there any duplicates in the dataset?")
+            duplicates = pandas_agent.run("Check for duplicates in the dataset")
             st.write(duplicates)
 
             st.write("**Data Summary**")
@@ -97,7 +104,7 @@ if st.session_state.clicked[1]:
             )
             st.write(correlation)
 
-            outliers = pandas_agent.run("Are there any outliers in the dataset?")
+            outliers = pandas_agent.run("Check for outliers in the dataset")
             st.write(outliers)
 
             new_features = pandas_agent.run(
@@ -116,12 +123,12 @@ if st.session_state.clicked[1]:
             st.write(summary_statistics)
 
             normality = pandas_agent.run(
-                f"Is {user_question_attribute} normally distributed? Do not create any charts."
+                f"Check if {user_question_attribute} is normally distributed. Do not create any charts."
             )
             st.write(normality)
 
             outliers = pandas_agent.run(
-                f"Are there any outliers in {user_question_attribute}?"
+                f"Check for outliers outliers in {user_question_attribute}"
             )
             st.write(outliers)
 
@@ -131,7 +138,7 @@ if st.session_state.clicked[1]:
             st.write(trends)
 
             missing_values = pandas_agent.run(
-                f"Are there any missing values in {user_question_attribute}?"
+                f"How many missing values  are there in {user_question_attribute}?"
             )
             st.write(missing_values)
 
@@ -139,11 +146,94 @@ if st.session_state.clicked[1]:
 
         @st.cache_data
         def function_question_dataframe():
-            dataframe_info = pandas_agent.run(
-                f"{user_question_dataframe}. You are not allowed to create any charts"
-            )
+            dataframe_info = pandas_agent.run(user_question_dataframe)
             st.write(dataframe_info)
             return
+
+        @st.cache_data
+        def google(prompt):
+            google = GoogleSearchAPIWrapper()
+            response = google.run(prompt)
+            return response
+
+        @st.cache_data
+        def prompt_templates():
+            data_problem_template = PromptTemplate(
+                input_variables=["business_problem"],
+                template="Convert the following business problem into a data problem: {business_problem}",
+            )
+            model_selection_template = PromptTemplate(
+                input_variables=["data_problem", "google_research"],
+                template="Give a list of ML algorithms that are suitable to solve this problem: {data_problem}, while using this google research: {google_research}",
+            )
+            return data_problem_template, model_selection_template
+
+        @st.cache_resource
+        def chains():
+            data_problem_prompt, model_selection_prompt = prompt_templates()
+            data_problem_chain = LLMChain(
+                llm=llm,
+                prompt=data_problem_prompt,
+                verbose=True,
+                output_key="data_problem",
+            )
+            model_selection_chain = LLMChain(
+                llm=llm,
+                prompt=model_selection_prompt,
+                verbose=True,
+                output_key="model_selection",
+            )
+            sequential_chain = SequentialChain(
+                chains=[data_problem_chain, model_selection_chain],
+                input_variables=["business_problem", "google_research"],
+                output_variables=["data_problem", "model_selection"],
+                verbose=True,
+            )
+            return sequential_chain
+
+        @st.cache_data
+        def chains_output(prompt, google_research):
+            chain = chains()
+            chain_output = chain(
+                {"business_problem": prompt, "google_research": google_research}
+            )
+            data_problem = chain_output["data_problem"]
+            model_selection_output = chain_output["model_selection"]
+            return data_problem, model_selection_output
+
+        @st.cache_data
+        def list_to_selectbox(model_selection_input):
+            algorithm_lines = model_selection_input.split("\n")
+            # Remove additional split if needed
+            algorithms = [
+                algorithm.split(":")[-1].split(".")[-1].strip()
+                for algorithm in algorithm_lines
+                if algorithm.strip()
+            ]
+            algorithms.insert(0, "Select an algorithm")
+            formatted_list_output = [
+                f"{algorithm}" for algorithm in algorithms if algorithm
+            ]
+            return formatted_list_output
+
+        @st.cache_resource
+        def python_agent():
+            agent_executor = create_python_agent(
+                llm=llm,
+                tool=PythonREPLTool(),
+                agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+                handle_parsing_errors=True,
+                # Add allow_dangerous_code=True to allow the agent to execute code
+            )
+            return agent_executor
+
+        @st.cache_data
+        def python_solution(data_problem, selected_algorithm, user_csv):
+            agent = python_agent()
+            solution = agent.run(
+                f"Write a python code to solve the following data problem: {data_problem} using the {selected_algorithm} algorithm on the dataset {user_csv}."
+            )
+            return solution
 
         # Main
         st.header("Exploratory Data Analysis")
@@ -166,7 +256,44 @@ if st.session_state.clicked[1]:
             user_question_dataframe = st.text_input(
                 "Is there anything else that you want to know about the data?"
             )
-            if user_question_dataframe is not None and user_question_dataframe != "":
+            if user_question_dataframe is not None and user_question_dataframe not in (
+                "no",
+                "No",
+                "",
+            ):
                 function_question_dataframe()
             if user_question_dataframe is ("no", "No"):
                 st.write("")
+
+            if user_question_dataframe:
+                st.divider()
+                st.header("Machine Learning")
+                st.write("Let's convert your business problem into a data problem.")
+
+                prompt = st.text_area(
+                    "What is the business problem that you would like to solve?"
+                )
+
+                if prompt:
+                    google_research = google(prompt)
+                    data_problem, model_selection_output = chains_output(
+                        prompt, google_research
+                    )
+
+                    st.write(data_problem)
+                    st.write(model_selection_output)
+
+                    formatted_list = list_to_selectbox(model_selection_output)
+                    selected_algorithm = st.selectbox(
+                        "Select an algorithm", formatted_list
+                    )
+
+                    if (
+                        selected_algorithm is not None
+                        and selected_algorithm != "Select an algorithm"
+                    ):
+                        st.subheader("Solution")
+                        solution = python_solution(
+                            data_problem, selected_algorithm, user_csv
+                        )
+                        st.write(solution)
